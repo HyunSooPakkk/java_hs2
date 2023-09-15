25ea6183dddc4ac8b39522ee8b7d24e7653c5045
KOREAVC <KOREAVC@DESKTOP-AI48KSJ>
2023-09-15 18:39:19 +0900

    Collections Framework / 하이마켓 프로그램 구현

--- p0914/src/p0914/Buyer.java
+++ p0914/src/p0914/Buyer.java
@@ -19,12 +19,20 @@ public class Buyer {
 	//컬렉션 선언
 	ArrayList list = new ArrayList(); //Collections Framework★★ 
 	
+	
 	//모든 종류의 상품(Tv, Computer, Audio)을 구매했을 때 사용하는 메소드
 	//int i = 0;
-	void buy(Product p) {
+	int buy(Product p) {
+		//상품 금액이 현재 잔액보다 큰 경우
+		if(money<p.price) {
+			System.out.println("※알림※ 잔액이 부족하여 구매 불가합니다.");
+			System.out.println();
+			return 0; //구매 실패
+		}//if
 		money -= p.price;
 		bonusPoint += p.bonusPoint;
 		list.add(p);
+		return 1; //구매 성공
 //		cart[i] = p; //p를 Product[] 배열에 담음.
 //		i++;
 		
